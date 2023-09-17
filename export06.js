54bd5899567e60b0dcd6bc6494368376327ae50f
kinganjum <anjumustad2@gmail.com>
2023-09-17 06:46:22 +0530

    done

---
+++ export06.js
@@ -0,0 +1,23 @@
+export const Logo = () => (
+    <a href="/">
+    <img className="logo"
+       alt="logo" src="https://lh3.googleusercontent.com/EbPS9tYwL3H8NkiYJPJbuRHmeIFVUYJs99IdfFT7ea6ZF9sw1Vhw9tiCFVCXqornHGKZyP8sy89up4G-yKqGzoLgPx8"/>
+    </a>
+);
+
+
+export const Header = function () {
+    return (
+    <div className="header">
+         <Logo/>         
+         <div className="nav-iteams">
+            <ul> 
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+                <li>Cart</li>
+            </ul>
+         </div>
+    </div>
+    );    
+};
